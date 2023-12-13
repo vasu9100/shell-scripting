4bb15384ddd062d7a57b9683b2eff0657a740c73
srinivas gonepudi <srigonepudi@gmail.com>
2023-12-13 11:37:32 +0530

    new file added

--- shell_scripting/fc.sh
+++ shell_scripting/fc.sh
@@ -4,6 +4,41 @@ ID=$(id -u)
 
 if [ $ID -ne 0 ]; then
     echo "ERROR yor are not a root user"
+    exit 1
 else
     echo "yes you ARE A ROOT USER"
-fi            
+fi 
+
+dnf install nginx -y
+
+if [$? -ne 0 ]; then
+
+    echo "Nginx Installation is failed"
+    exit 1
+else
+
+    echo "Nginx Installation is success"  
+fi
+
+systemctl enable nginx
+
+if [$? -ne 0 ]; then
+
+    echo "Nginx enabled failed"
+    exit 1
+else
+
+    echo "Nginx Nginx enabled success"  
+fi
+
+systemctl start nginx
+
+if [$? -ne 0 ]; then
+
+    echo "Nginx unable to start failed"
+    exit 1
+else
+
+    echo "Nginx start is success"  
+fi
+
